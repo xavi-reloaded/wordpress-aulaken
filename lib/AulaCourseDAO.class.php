fe888b6cb7997ca7d7cfbcf10fad01d1ee83f5c2
Xavi Hidalgo <xavi.hidalgo.fernandez@gmail.com>
2013-03-26 11:52:28 +0100

    fixed: bad required path

--- lib/AulaCourseDAO.class.php
+++ lib/AulaCourseDAO.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__) . '/AulaGenericItem.class.php');
+require_once(dirname(__FILE__) . '/AulaCourseItem.class.php');
 require_once(dirname(__FILE__) . '/AulaHelper.class.php');
 
 /**
